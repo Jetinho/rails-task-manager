b40b47043f4caeb842d0e61e38bd8db4cdd9a41f
Julien Jet <jet.julien@gmail.com>
2016-11-22 15:39:15 -0200

    Add destroy redirection and dry update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,12 +16,12 @@ class TasksController < ApplicationController
   def edit
   end
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
   def destroy
     @task.destroy
+    redirect_to tasks_path
   end
 
   private
