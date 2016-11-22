b4bb4f01e86fc2bfc6eab52eda5ef1460661b650
Julien Jet <jet.julien@gmail.com>
2016-11-22 12:31:35 -0200

    Add task routes index action and new/edit actions incomplete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+  def show
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+end
